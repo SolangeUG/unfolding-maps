33d4dbcd90f0effc4648358c3f34b8d7100b7089
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2017-12-24 15:15:45 +0100

    Implement module 3 tasks

--- src/main/java/module3/EarthquakeCityMap.java
+++ src/main/java/module3/EarthquakeCityMap.java
@@ -1,78 +1,86 @@
 package module3;
 
-//Java utilities libraries
-import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.Comparator;
-import java.util.List;
-
-//Processing library
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import de.fhpotsdam.unfolding.providers.AbstractMapProvider;
+import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
+import de.fhpotsdam.unfolding.providers.Microsoft;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import parsing.ParseFeed;
 import processing.core.PApplet;
 
-//Unfolding libraries
-import de.fhpotsdam.unfolding.UnfoldingMap;
-import de.fhpotsdam.unfolding.marker.Marker;
-import de.fhpotsdam.unfolding.data.PointFeature;
-import de.fhpotsdam.unfolding.marker.SimplePointMarker;
-import de.fhpotsdam.unfolding.providers.Google;
-import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
-import de.fhpotsdam.unfolding.utils.MapUtils;
+import java.util.ArrayList;
+import java.util.List;
 
-//Parsing library
-import parsing.ParseFeed;
 
-/** EarthquakeCityMap
+/**
+ * EarthquakeCityMap
+ *
  * An application with an interactive map displaying earthquake data.
- * Author: UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
- * Date: July 17, 2015
- * */
+ * Co-author: UC San Diego Intermediate Software Development MOOC team
+ *
+ * @author Solange U. Gasengayire
+ *
+ */
 public class EarthquakeCityMap extends PApplet {
 
 	// You can ignore this.  It's to keep eclipse from generating a warning.
 	private static final long serialVersionUID = 1L;
 
-	// IF YOU ARE WORKING OFFLINE, change the value of this variable to true
+	// If you are working offline, change the value of this variable to true
 	private static final boolean offline = false;
 	
 	// Less than this threshold is a light earthquake
-	public static final float THRESHOLD_MODERATE = 5;
+	private static final float THRESHOLD_MODERATE = 5;
 	// Less than this threshold is a minor earthquake
-	public static final float THRESHOLD_LIGHT = 4;
+	private static final float THRESHOLD_LIGHT = 4;
 
-	/** This is where to find the local tiles, for working without an Internet connection */
-	public static String mbTilesString = "blankLight-1-3.mbtiles";
-	
 	// The map
 	private UnfoldingMap map;
-	
-	//feed with magnitude 2.5+ Earthquakes
-	private String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
 
-	
+
 	public void setup() {
-		size(950, 600, OPENGL);
+
+		int canvasWidth = 1520;
+		int canvasHeight = 900;
+		size(canvasWidth, canvasHeight, OPENGL);
+
+		String earthquakesURL = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.atom";
 
 		if (offline) {
-		    map = new UnfoldingMap(this, 200, 50, 700, 500, new MBTilesMapProvider(mbTilesString));
+			String mbTilesString = "blankLight-1-3.mbtiles";
+			map = new UnfoldingMap(this, 200, 50, 700, 500, new MBTilesMapProvider(mbTilesString));
 		    earthquakesURL = "2.5_week.atom"; 	// Same feed, saved Aug 7, 2015, for working offline
-		}
-		else {
-			map = new UnfoldingMap(this, 200, 50, 700, 500, new Google.GoogleMapProvider());
-			// IF YOU WANT TO TEST WITH A LOCAL FILE, uncomment the next line
-			//earthquakesURL = "2.5_week.atom";
+		} else {
+			int mapX = 120, mapY = 50;
+			int mapWidth = 1680, mapHeight = 800;
+			AbstractMapProvider provider;
+			provider = new Microsoft.RoadProvider();
+			//provider = new OpenStreetMap.OpenStreetMapProvider();
+
+			map = new UnfoldingMap(this, mapX, mapY, mapWidth, mapHeight, provider);
+			// If you want to test with a local file, uncomment the next line
+			// earthquakesURL = "2.5_week.atom";
 		}
 		
 	    map.zoomToLevel(2);
 	    MapUtils.createDefaultEventDispatcher(this, map);	
 			
 	    // The List you will populate with new SimplePointMarkers
-	    List<Marker> markers = new ArrayList<Marker>();
+	    List<Marker> markers = new ArrayList<>();
 
-	    //Use provided parser to collect properties for each earthquake
-	    //PointFeatures have a getLocation method
-	    List<PointFeature> earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
-	    
+	    // Use provided parser to collect properties for each earthquake
+	    // PointFeatures have a getLocation method
+		List<PointFeature> earthquakes;
+		try {
+			earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
+		} catch (Exception e) {
+			earthquakesURL = "2.5_week.atom";
+			earthquakes = ParseFeed.parseEarthquake(this, earthquakesURL);
+		}
+
 	    // These print statements show you (1) all of the relevant properties 
 	    // in the features, and (2) how to get one property and use it
 	    if (earthquakes.size() > 0) {
@@ -81,36 +89,105 @@ public class EarthquakeCityMap extends PApplet {
 	    	Object magObj = f.getProperty("magnitude");
 	    	float mag = Float.parseFloat(magObj.toString());
 	    	// PointFeatures also have a getLocation method
+			System.out.println(f.getLocation() + " | " + mag);
 	    }
 	    
-	    // Here is an example of how to use Processing's color method to generate 
-	    // an int that represents the color yellow.  
-	    int yellow = color(255, 255, 0);
-	    
-	    //TODO: Add code here as appropriate
+	    // DONE: Add code here as appropriate
+		for (PointFeature feature: earthquakes) {
+			Marker marker = createMarker(feature);
+			markers.add(marker);
+		}
+		map.addMarkers(markers);
+
 	}
-		
-	// A suggested helper method that takes in an earthquake feature and 
-	// returns a SimplePointMarker for that earthquake
-	// TODO: Implement this method and call it from setUp, if it helps
-	private SimplePointMarker createMarker(PointFeature feature)
-	{
-		// finish implementing and use this method, if it helps.
-		return new SimplePointMarker(feature.getLocation());
+
+	/**
+	 * Helper method that returns a SimplePointMaker
+	 * for an earthquake feature received as parameter
+	 * @param feature The earthquake feature to build a marker for
+	 * @return the point marker for the feature parameter
+	 */
+	private SimplePointMarker createMarker(PointFeature feature) {
+		// DONE: finish implementing and use this method, if it helps.
+
+		int color = color(255, 0, 0);
+		int radius = 15;
+		float magnitude = getMagnitude(feature);
+		if (magnitude < THRESHOLD_LIGHT) {
+			color = color(0, 0, 255);
+			radius = 5;
+		} else if (magnitude < THRESHOLD_MODERATE) {
+			color = color(255, 255, 0);
+			radius = 10;
+		}
+
+		SimplePointMarker marker = new SimplePointMarker(
+				feature.getLocation(), feature.getProperties()
+		);
+		marker.setRadius(radius);
+		marker.setColor(color);
+		return marker;
 	}
-	
+
+	/**
+	 * Draw a map and its markers
+	 */
 	public void draw() {
 	    background(10);
 	    map.draw();
 	    addKey();
 	}
 
-
-	// helper method to draw key in GUI
-	// TODO: Implement this method to draw the key
-	private void addKey() 
-	{	
+	/**
+	 * Helper method to draw the key in the GUI
+	 * DONE: Implement this method to draw the key
+	 */
+	private void addKey() {
 		// Remember you can use Processing's graphics methods here
-	
+
+		// Key canvas
+		fill(color(255, 253, 231));
+		float keyX = 100, keyY = 220;
+		float keyWidth = 250, keyHeight = 500;
+		rect(keyX, keyY, keyWidth, keyHeight);
+
+		// Key title
+		fill(0, 102, 153, 204);
+		textSize(24);
+		text("Earthquake Key", 130, 260);
+
+		// Legend for red markers
+		fill(255, 0, 0, 204);
+		ellipse(145, 320, 20, 20);
+		fill(0, 0, 0, 204);
+		textSize(16);
+		text("5.0+ Magnitude", 170, 328);
+
+		// Legend for yellow markers
+		fill(255, 255, 0, 204);
+		ellipse(145, 400, 15, 15);
+		fill(0, 0, 0, 204);
+		textSize(16);
+		text("4.0+ Magnitude", 170, 408);
+
+		// Legend for blue markers
+		fill(0, 0, 255, 204);
+		ellipse(145, 480, 10, 10);
+		fill(0, 0, 0, 204);
+		textSize(16);
+		text("< 4.0 Magnitude", 170, 488);
+
+	}
+
+	/**
+	 * Return the magnitude from a point feature properties
+	 * @param feature The point feature to search from
+	 * @return the magnitude from its properties
+	 */
+	private float getMagnitude(PointFeature feature) {
+		Object magObj = feature.getProperty("magnitude");
+		float magnitude;
+		magnitude = Float.parseFloat(magObj.toString());
+		return magnitude;
 	}
 }
